875f6436ccd0d50d7175eda09f5cdf5916248b9f
Shaddy <shadybett540@gmail.com>
2024-02-17 22:07:38 +0300

    fixed typos in models file

--- models.py
+++ models.py
@@ -4,21 +4,21 @@ db = SQLAlchemy()
 
 class Admin(db.Model):
     __tablename__='admins'
-    id = db.Column(db.Integer,Primary_key=True)
+    id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String,nullable=False)
     email = db.Column(db.String,nullable=False)
     password = db.Column(db.String,nullable=False)
 
 class Teacher(db.Model):
     __tablename__='teachers'
-    id = db.Column(db.Integer,Primary_key=True)
+    id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String,nullable=False)
     email = db.Column(db.String,nullable=False)
     password = db.Column(db.String,nullable=False)
 
 class Student(db.Model):
     __tablename__='students'
-    id = db.Column(db.String,Primary_key=True)
+    id = db.Column(db.String,primary_key=True)
     username = db.Column(db.String,nullable=False)
     email = db.Column(db.String,nullable=False)
     password = db.Column(db.String,nullable=False)
