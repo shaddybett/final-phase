fd6718a43d8afefaff4bb9466c6a8624e8e7ae05
Shaddy <shadybett540@gmail.com>
2024-02-17 21:54:47 +0300

    declared flask

--- app.py
+++ app.py
@@ -4,38 +4,38 @@ from flask_migrate import Migrate
 from models import db,Admin,Teacher,Student
 
 app = Flask(__name__)
-api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///final.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
+# api = Api(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///final.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db.init_app(app)
 
-@app.route('login',methods=['GET'])
-def login():
-    data = request.get_json()
-    email = data.get('email')
-    existing_user = Admin.query.filter_by(email=email).first()
-    if existing_user:
-        return jsonify({'message':'Login successful'}),200
-    else :
-        return jsonify ({'error':'Invalid details'}),404
+# @app.route('login',methods=['GET'])
+# def login():
+#     data = request.get_json()
+#     email = data.get('email')
+#     existing_user = Admin.query.filter_by(email=email).first()
+#     if existing_user:
+#         return jsonify({'message':'Login successful'}),200
+#     else :
+#         return jsonify ({'error':'Invalid details'}),404
 
 
-@app.route('login',methods=['GET'])
-def login():
-    data = request.get_json()
-    email = data.get('email')
-    existing_user = Teacher.query.filter_by(email=email).first()
-    if existing_user:
-        return jsonify({'message':'Login successful'}),200
-    else :
-        return jsonify ({'error':'Invalid details'}),404
+# @app.route('login',methods=['GET'])
+# def login():
+#     data = request.get_json()
+#     email = data.get('email')
+#     existing_user = Teacher.query.filter_by(email=email).first()
+#     if existing_user:
+#         return jsonify({'message':'Login successful'}),200
+#     else :
+#         return jsonify ({'error':'Invalid details'}),404
 
-@app.route('login',methods=['GET'])
-def login():
-    data = request.get_json()
-    email = data.get('email')
-    existing_user = Student.query.filter_by(email=email).first()
-    if existing_user:
-        return jsonify({'message':'Login successful'}),200
-    else :
-        return jsonify ({'error':'Invalid details'}),404
+# @app.route('login',methods=['GET'])
+# def login():
+#     data = request.get_json()
+#     email = data.get('email')
+#     existing_user = Student.query.filter_by(email=email).first()
+#     if existing_user:
+#         return jsonify({'message':'Login successful'}),200
+#     else :
+#         return jsonify ({'error':'Invalid details'}),404
