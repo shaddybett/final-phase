00e77ab19c6346ab3917cd22db17efb3dcab6aaf
Shaddy <shadybett540@gmail.com>
2024-02-17 22:24:51 +0300

    added new admin to the db

--- server/seed.py
+++ server/seed.py
@@ -3,8 +3,8 @@ from server.models import Admin
 
 with app.app_context():
     admins = [Admin(
-        username='Bett',
-        email='bett@gmail.com',
+        username='Kai',
+        email='kai@gmail.com',
         password='12345'
     )]
     db.session.add_all(admins)
