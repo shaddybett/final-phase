4c5a225c10fd779d0ea6bf71b7a25aa6f6abffe4
Shaddy <shadybett540@gmail.com>
2024-02-17 21:54:12 +0300

    done

---
+++ models.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Admin(db.Model):
+    __tablename__='admins'
+    id = db.Column(db.Integer,Primary_key=True)
+    username = db.Column(db.String,nullable=False)
+    email = db.Column(db.String,nullable=False)
+    password = db.Column(db.String,nullable=False)
+
+class Teacher(db.Model):
+    __tablename__='teachers'
+    id = db.Column(db.Integer,Primary_key=True)
+    username = db.Column(db.String,nullable=False)
+    email = db.Column(db.String,nullable=False)
+    password = db.Column(db.String,nullable=False)
+
+class Student(db.Model):
+    __tablename__='students'
+    id = db.Column(db.String,Primary_key=True)
+    username = db.Column(db.String,nullable=False)
+    email = db.Column(db.String,nullable=False)
+    password = db.Column(db.String,nullable=False)
